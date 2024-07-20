d09ebf7094cd91ff66ebd2919edd5e68e9f75a89
Hayden Foley <hpfoley1@gmail.com>
2024-07-20 15:13:01 -0400

    Holiday and missing location fix

--- src/client.rs
+++ src/client.rs
@@ -4,7 +4,10 @@ use scraper::Html;
 
 use crate::date::DateId;
 use crate::error::WebGetError;
+use crate::html;
 use crate::lectionary::Lectionary;
+
+const BASE_URL: &str = "https://bible.usccb.org";
 
 /// Client for interacting with the USCCB site
 #[derive(Default, Clone)]
@@ -15,6 +18,19 @@ pub struct WebClient {
 impl WebClient {
     pub async fn get_for_date_id(&self, date_id: DateId) -> Result<Lectionary, WebGetError> {
         let url = Self::url_for_date(&date_id);
+        let document = self.get_document_from_url(url).await?;
+
+        if let Some(endpoint) = html::get_holiday_day_reading_link(&document) {
+            info!("{date_id} seems to be a holiday. Using the link for the daytime reading");
+            let url = Self::url_for_endpoint(endpoint);
+            let document = self.get_document_from_url(url).await?;
+            return Lectionary::create_from_html(date_id, &document).map_err(WebGetError::ParseError)
+        }
+
+        Lectionary::create_from_html(date_id, &document).map_err(WebGetError::ParseError)
+    }
+
+    async fn get_document_from_url(&self, url: Url) -> Result<Html, WebGetError> {
         debug!("Sending GET request to {}", url);
         let response = self.client.get(url).send().await.map_err(WebGetError::ClientError)?;
         if !response.status().is_success() {
@@ -22,12 +38,38 @@ impl WebClient {
         }
 
         let response_text = response.text().await.map_err(WebGetError::ResponseError)?;
-        let document = Html::parse_document(&response_text);
-        Lectionary::create_from_html(date_id, &document).map_err(WebGetError::ParseError)
+        Ok(Html::parse_document(&response_text))
     }
 
     fn url_for_date(date_id: &DateId) -> Url {
-        let url_string = format!("https://bible.usccb.org/bible/readings/{date_id}.cfm");
+        let url_string = format!("{BASE_URL}/bible/readings/{date_id}.cfm");
         Url::parse(&url_string).expect("Formmatted string is valid URL")
+    }
+
+    fn url_for_endpoint(endpoint: &str) -> Url {
+        let mut url_string = String::new();
+        url_string.push_str(BASE_URL);
+        url_string.push_str(endpoint);
+        Url::parse(&url_string).expect("Base URL plus endpoint must be valid URL")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_url_for_date() {
+        let date_id = DateId::checked_from_str("072024").unwrap();
+        let url = WebClient::url_for_date(&date_id);
+        assert_eq!(url.origin().ascii_serialization(), BASE_URL);
+        assert_eq!(url.path(), "/bible/readings/072024.cfm");
+    }
+
+    #[test]
+    fn correct_url_for_endpoint() {
+        let url = WebClient::url_for_endpoint("/example/endpoint");
+        assert_eq!(url.origin().ascii_serialization(), BASE_URL);
+        assert_eq!(url.path(), "/example/endpoint");
     }
 }
--- src/lectionary.rs
+++ src/lectionary.rs
@@ -1,47 +1,10 @@
 use std::fmt::{self, Display, Formatter};
-use std::sync::OnceLock;
 
 use log::*;
-use scraper::element_ref::ElementRef;
-use scraper::selectable::Selectable;
-use scraper::selector::Selector;
-use scraper::Html;
 
 use crate::date::DateId;
 use crate::db::{LectionaryDbEntity, ReadingRow};
-use crate::error::{LectionaryHtmlError, ReadingHtmlError, ReadingNameFromStringError};
-use crate::html;
-
-/// Main container in which all other relevant elements are found
-fn container_selector() -> &'static Selector {
-    static CONTAINER_SELECTOR: OnceLock<Selector> = OnceLock::new();
-    CONTAINER_SELECTOR.get_or_init(|| Selector::parse("#block-usccb-readings-content div.page-container").unwrap())
-}
-/// Use within element found by `CONTAINER_SELECTOR`. Finds the element that has the name of the day (e.g. Fourteenth Sunday in Ordinary Time )
-fn day_name_selector() -> &'static Selector {
-    static DAY_NAME_SELECTOR: OnceLock<Selector> = OnceLock::new();
-    DAY_NAME_SELECTOR.get_or_init(|| Selector::parse("div.b-lectionary div.innerblock :first-child").unwrap())
-}
-/// Use within element found by `CONTAINER_SELECTOR`. Finds all the verse(aka reading) containers
-fn readings_selector() -> &'static Selector {
-    static READINGS_SELECTOR: OnceLock<Selector> = OnceLock::new();
-    READINGS_SELECTOR.get_or_init(|| Selector::parse("div.b-verse").unwrap())
-}
-/// Use within a element found by `READINGS_SELECTOR`
-fn reading_name_selector() -> &'static Selector {
-    static READING_NAME_SELECTOR: OnceLock<Selector> = OnceLock::new();
-    READING_NAME_SELECTOR.get_or_init(|| Selector::parse(".name").unwrap())
-}
-/// Use within element found by `READINGS_SELECTOR`. The container with the actual text of the reading.
-fn reading_content_selector() -> &'static Selector {
-    static READING_CONTENT_SELECTOR: OnceLock<Selector> = OnceLock::new();
-    READING_CONTENT_SELECTOR.get_or_init(|| Selector::parse("div.content-body").unwrap())
-}
-/// Use within element found by `READINGS_SELECTOR`. Finds the address (book, chapter, verse(s)) of the reading
-fn reading_location_selector() -> &'static Selector {
-    static READING_LOCATION_SELECTOR: OnceLock<Selector> = OnceLock::new();
-    READING_LOCATION_SELECTOR.get_or_init(|| Selector::parse("div.content-header div.address a").unwrap())
-}
+use crate::error::ReadingNameFromStringError;
 
 #[derive(Debug)]
 pub struct Lectionary {
@@ -54,34 +17,17 @@ pub struct Lectionary {
 }
 
 impl Lectionary {
-    pub fn create_from_html(id: DateId, document: &Html) -> Result<Self, LectionaryHtmlError> {
-        let container = document
-            .select(container_selector())
-            .next()
-            .ok_or(LectionaryHtmlError::NoContainerFound)?;
-        let day_name_elmnt = container
-            .select(day_name_selector())
-            .next()
-            .ok_or(LectionaryHtmlError::NoDayNameElementFound)?;
-        let day_name = day_name_elmnt.inner_html().trim().to_owned();
-
-        let readings = ParsedReadings::extract_from_container(container);
-        let reading_1 = readings
-            .reading_1
-            .ok_or(LectionaryHtmlError::MissingReading(ReadingName::Reading1))?;
-        let reading_2 = readings.reading_2;
-        let resp_psalm = readings.resp_psalm.ok_or(LectionaryHtmlError::MissingReading(ReadingName::Psalm))?;
-        let gospel = readings.gospel.ok_or(LectionaryHtmlError::MissingReading(ReadingName::Gospel))?;
-
-        Ok(Lectionary {
+    pub fn new(id: DateId, day_name: String, reading_1: Reading, reading_2: Option<Reading>, resp_psalm: Reading, gospel: Reading) -> Self {
+        Self {
             id,
             day_name,
             reading_1,
             reading_2,
             resp_psalm,
             gospel,
-        })
+        }
     }
+
     pub fn get_id(&self) -> &DateId {
         &self.id
     }
@@ -112,44 +58,6 @@ impl From<LectionaryDbEntity> for Lectionary {
             resp_psalm: Reading::from(entity.psalm_row),
             gospel: Reading::from(entity.gospel_row),
         }
-    }
-}
-
-/// For temporary use while constructing a `Lectionary` from html
-#[derive(Default)]
-struct ParsedReadings {
-    reading_1: Option<Reading>,
-    reading_2: Option<Reading>,
-    resp_psalm: Option<Reading>,
-    gospel: Option<Reading>,
-}
-
-impl ParsedReadings {
-    fn extract_from_container(container: ElementRef<'_>) -> Self {
-        let mut out = ParsedReadings::default();
-
-        let readings = container.select(readings_selector());
-        for reading_elmt in readings {
-            if let Some(name_elmnt) = reading_elmt.select(reading_name_selector()).next() {
-                match ReadingName::try_from(html::replace_entities(name_elmnt.inner_html())) {
-                    Ok(name) => match Reading::from_container(reading_elmt) {
-                        Ok(reading) => match name {
-                            ReadingName::Reading1 => out.reading_1 = Some(reading),
-                            ReadingName::Reading2 => out.reading_2 = Some(reading),
-                            ReadingName::Psalm => out.resp_psalm = Some(reading),
-                            ReadingName::Gospel => out.gospel = Some(reading),
-                            ReadingName::Alleluia => {}
-                        },
-                        Err(e) => error!("Failed to process element '{}'; Reason: {}", name, e),
-                    },
-                    Err(e) => warn!("Unable to identify reading name: {}", e),
-                };
-            } else {
-                error!("Found reading element with no name element");
-            }
-        } // END FOR LOOP
-
-        out
     }
 }
 
@@ -206,27 +114,16 @@ pub struct Reading {
     text: String,
 }
 impl Reading {
+    pub fn new(location: String, text: String) -> Self {
+        Self { location, text }
+    }
+
     pub fn get_location(&self) -> &str {
         &self.location
     }
 
     pub fn get_text(&self) -> &str {
         &self.text
-    }
-
-    fn from_container(reading_container: ElementRef<'_>) -> Result<Self, ReadingHtmlError> {
-        let location_elmt = reading_container
-            .select(reading_location_selector())
-            .next()
-            .ok_or(ReadingHtmlError::MissingLocation)?;
-        let location = html::replace_entities(location_elmt.inner_html());
-        let content = reading_container
-            .select(reading_content_selector())
-            .next()
-            .ok_or(ReadingHtmlError::MissingContent)?;
-        let text = html::element_to_plain_text(&content);
-
-        Ok(Reading { location, text })
     }
 }
 impl From<ReadingRow> for Reading {
@@ -235,25 +132,5 @@ impl From<ReadingRow> for Reading {
             location: row.location,
             text: row.content,
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::{fs::File, io::Read, path::PathBuf, str::FromStr};
-
-    #[test]
-    fn derialize_sunday_lectionary() {
-        let mut html_string = String::new();
-        File::open(PathBuf::from_str("tests/resources/sunday_or.html").unwrap())
-            .unwrap()
-            .read_to_string(&mut html_string)
-            .unwrap();
-
-        let html_doc = Html::parse_document(&html_string);
-        let lectionary = Lectionary::create_from_html(DateId::today(), &html_doc).unwrap();
-        assert_eq!(DateId::today(), lectionary.id);
-        assert!(lectionary.reading_2.is_some());
     }
 }
